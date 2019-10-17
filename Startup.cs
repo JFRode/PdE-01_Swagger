4256fb389429951415c395938eae940c31d60dc0
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2019-10-17 15:49:18 -0300

    CM - Dependencies and configuration

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using PdE_01_BrazilianCitiesAPI.Infrastructure.Contexts;
+using PdE_01_BrazilianCitiesAPI.Repository;
 
 namespace PdE_01_BrazilianCitiesAPI
 {
@@ -26,6 +22,11 @@ namespace PdE_01_BrazilianCitiesAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<BrazilianCitiesApiDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddTransient<ICityRepository, CityRepository>();
+            services.AddTransient<IStateRepository, StateRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
