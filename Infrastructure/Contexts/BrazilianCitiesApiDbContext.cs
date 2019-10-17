8842807f9639343a830458cc2aaeee7d7930c00e
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2019-10-17 15:47:39 -0300

    CM - EF Migrations and Context

---
+++ Infrastructure/Contexts/BrazilianCitiesApiDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PdE_01_BrazilianCitiesAPI.Models;
+
+namespace PdE_01_BrazilianCitiesAPI.Infrastructure.Contexts
+{
+    public class BrazilianCitiesApiDbContext : DbContext
+    {
+        public DbSet<State> States { get; set; }
+        public DbSet<City> Cities { get; set; }
+
+        public BrazilianCitiesApiDbContext(DbContextOptions<BrazilianCitiesApiDbContext> options) : base(options)
+        {
+        }
+    }
+}
