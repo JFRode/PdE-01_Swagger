b5b314a48804ef3c2d351704a0509ea1c160e095
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2019-10-17 15:34:05 -0300

    CM - Models added (City and State)

---
+++ Models/City.cs
@@ -0,0 +1,9 @@
+﻿namespace PdE_01_BrazilianCitiesAPI.Models
+{
+    public class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public State State { get; set; }
+    }
+}
