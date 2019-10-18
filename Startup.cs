1cfad421083660402ae3819a499745be4d6a0a6a
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2019-10-18 10:09:33 -0300

    CM - Swagger configured

--- Startup.cs
+++ Startup.cs
@@ -4,8 +4,10 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using PdE_01_BrazilianCitiesAPI.Infrastructure.Contexts;
 using PdE_01_BrazilianCitiesAPI.Repository;
+using System;
 
 namespace PdE_01_BrazilianCitiesAPI
 {
@@ -18,7 +20,6 @@ namespace PdE_01_BrazilianCitiesAPI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
@@ -27,9 +28,29 @@ namespace PdE_01_BrazilianCitiesAPI
 
             services.AddTransient<ICityRepository, CityRepository>();
             services.AddTransient<IStateRepository, StateRepository>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "BrazilianCitiesApi",
+                    Version = "v1",
+                    Description = "A simple example ASP.NET Core Web API",
+                    TermsOfService = new Uri("https://github.com/JFRode/PdE-01_Swagger/blob/master/LICENSE"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "João Felipe Gonçalves",
+                        Email = string.Empty,
+                        Url = new Uri("https://github.com/jfrode"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under MIT",
+                        Url = new Uri("https://github.com/JFRode/PdE-01_Swagger/blob/master/LICENSE"),
+                    }
+                });
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -46,6 +67,12 @@ namespace PdE_01_BrazilianCitiesAPI
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "BrazilianCitiesApi v1");
             });
         }
     }
